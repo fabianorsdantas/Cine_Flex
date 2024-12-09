b13d0caa7dac833e6eb2ecddfa1f68766c615847
Fabiano_Dantas <fabianorsdantas@gmail.com>
2024-12-09 10:21:43 -0300

    Cores dos cards

---
+++ src/constants/colors.jsx
@@ -0,0 +1,15 @@
+export const COLORS = {
+    cyan: {
+      border: "#0E7D71",
+      fill: "#1AAE9E"
+    },
+    grey: {
+      border: "#808F9D",
+      fill: "#C3CFD9"
+    },
+    yellow: {
+      border: "#F7C52B",
+      fill: "#FBE192"
+    }
+  };
+  
