da1ab664c92fc6ce5e809e911e0eb3b0e02e332d
Fabiano_Dantas <fabianorsdantas@gmail.com>
2024-12-09 16:13:25 -0300

    Resolvendo erros

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import App from "./App";
+import App from "./App.jsx";
 import { GlobalStyle } from "./styles/GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -11,3 +10,19 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+/*
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import "./assets/css/style.css"
+
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'milligram/dist/milligram.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+    <App />
+  
+)*/
