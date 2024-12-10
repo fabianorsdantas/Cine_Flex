834191bb446a4c3cc3ca70c131922b389346c504
Fabiano_Dantas <fabianorsdantas@gmail.com>
2024-12-10 16:32:16 -0300

    Remover itens desnecessários

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,7 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
-import arrow from "../assets/arrow.svg";
+//import arrow from "../assets/arrow.svg";
 
 export default function Banner({ setBuyers }) {
   const location = useLocation();
@@ -11,36 +11,30 @@ export default function Banner({ setBuyers }) {
     setBuyers([]);
     navigate(-1);
   }
-
+  
   return (
     <Header>
-      {location.pathname !== "/" && <img src={arrow} alt="back arrow" onClick={goBack} />}
-      <Link to="/">
+     <Link to="/">
         <h1>CineFlex</h1>
       </Link>
     </Header>
   );
+  
 }
 
 const Header = styled.header`
   width: 100vw;
   height: 67px;
-
-  background: #EE897F;
+  background: #ee897f;
   display: flex;
-  justify-content: center;
+  justify-content: center; /* Distribui os itens entre as extremidades */
   align-items: center;
-
   position: sticky;
   top: 0;
   left: 0;
+  padding: 0 20px; /* Espaçamento lateral */
 
-  img {
-    width: 35px;
-    position: fixed;
-    left: 3vw;
-  }
-
+  /* Alinha o conteúdo do Header */
   h1 {
     font-family: Raleway;
     font-size: 34px;
@@ -50,5 +44,11 @@ const Header = styled.header`
     text-underline-position: from-font;
     text-decoration-skip-ink: none;
     color: #FADBC5;
+    margin: 0; /* Remove qualquer margem automática */;
+    &:hover {
+    color: #0056b3;
   }
+  }
+
 `;
+
